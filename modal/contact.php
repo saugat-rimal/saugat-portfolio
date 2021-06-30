40e08f529269ae93cef21cd67d0fcfc0d9ba531d
Saugat Rimal <saugatrimal60@gmail.com>
2021-06-30 11:59:54 +0000

    Update contact.php

--- modal/contact.php
+++ modal/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 // Put contacting email here
-$php_main_email = "yourmail@gmail.com";
+$php_main_email = "saugatrimal60@gmail.com";
 
 //Fetching Values from URL
 $php_name = $_POST['ajax_name'];
